c07e1360e9f1207b4cdfc5a9d8f85128bef08b7b
Dominique <batistadominique@hotmail.com>
2019-09-27 14:08:22 +0100

    switched chart.js to Chart.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ require.config({
     paths: {
         'angular': '../node_modules/angular/angular.min',
         'angular-route': '../node_modules/angular-route/angular-route',
-        'chart': '../node_modules/chart.js/dist/chart.min',
+        'chart': '../node_modules/chart.js/dist/Chart.min',
         'angular-chart': '../node_modules/angular-chart.js/dist/angular-chart.min',
         'jquery': '../node_modules/jquery/dist/jquery',
         'bootstrap': '../node_modules/bootstrap/dist/js/bootstrap.min',

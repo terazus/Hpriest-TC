4795818b6307aef31e1526df191fac8d8fc086ce
Terazus <batistadominique@hotmail.com>
2019-09-29 09:05:12 -0700

    Organized graphs

--- app/graphs/graphsCtrl.js
+++ app/graphs/graphsCtrl.js
@@ -1,9 +1,87 @@
 define([], function () {
     let graphsController = function ($scope) {
-        $scope.test = "abc";
 
-        function computeGraph(spell){
-            console.log(spell);
+        /* GRAPH OPTIONS */
+        $scope.spell_selected = false;
+        $scope.graphParam = {
+            min: 0,
+            max: 1500,
+            step: 10
+        };
+
+        // Setting X labels
+        for (let i=$scope.graphParam.min;
+             i<=$scope.graphParam.max;
+             i+=$scope.graphParam.step){
+
+        }
+
+        $scope.onClick = function (points, evt) {
+            console.log(points, evt);
+        };
+
+        $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }];
+        $scope.options = {
+            scales: {
+                yAxes: [
+                    {
+                        id: 'y-axis-1',
+                        type: 'linear',
+                        display: true,
+                        position: 'left'
+                    }
+                ]
+            }
+        };
+
+        $scope.computeGraph = function(spell){
+            $scope.spell_selected = spell;
+            $scope.graphs = {
+                'labels': [],
+                'series': [],
+                data: {
+                    'HPS': [],
+                    'HPM': [],
+                    'HPS/M': [],
+                    'HES': []
+                }
+            };
+
+            // Setting series
+            let rankIndex = 1;
+            for (let rank of spell['ranks']){
+
+                $scope.graphs.series.push("RANK " + rankIndex);
+
+                let rankHPSM = [];
+                let rankHPM = [];
+                let rankHPS = [];
+                let rankHES = [];
+
+                for (let i=$scope.graphParam.min;
+                     i<=$scope.graphParam.max;
+                     i+=$scope.graphParam.step){
+
+                    if (rankIndex === 1){
+                        $scope.graphs.labels.push(i);
+                    }
+                    let HPS_M = $scope.computeHPSM(i, spell, rankIndex-1);
+                    let HPM = $scope.computeHPM(i, spell, rankIndex-1);
+                    let HPS = $scope.computeHPS(i, spell, rankIndex-1);
+                    let HES = $scope.computeSpecial(i, spell, rankIndex-1, [1, 1]);
+
+                    rankHPSM.push(HPS_M);
+                    rankHPM.push(HPM);
+                    rankHPS.push(HPS);
+                    rankHES.push(HES);
+
+                }
+                $scope.graphs.data['HPS/M'].push(rankHPSM);
+                $scope.graphs.data['HPM'].push(rankHPM);
+                $scope.graphs.data['HPS'].push(rankHPS);
+                $scope.graphs.data['HES'].push(rankHES);
+                rankIndex ++;
+            }
         }
 
 
--- app/home/dataCtrl.js
+++ app/home/dataCtrl.js
@@ -6,36 +6,7 @@ define([], function () {
             url: data_file
         };
 
-        $scope.graphs = {
-          "Greater Heal": {
-            'series': [],
-            'data': []
-          },
-          "labels" : []
-        };
-
-        // Setting X labels
-        for (let i=0;i<=1000;i+=10){
-          $scope.graphs.labels.push(i)
-        }
-
-        $scope.onClick = function (points, evt) {
-          console.log(points, evt);
-        };
-        $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }];
-        $scope.options = {
-        scales: {
-          yAxes: [
-            {
-              id: 'y-axis-1',
-              type: 'linear',
-              display: true,
-              position: 'left'
-            }
-          ]
-        }
-      };
-
+        /* TRIGGERING QUERY */
 
         $http(request).then(function(response){
 
@@ -46,7 +17,7 @@ define([], function () {
 
             for (let spellIt in response.data['spells']){
 
-                let currentSpell = response.data['spells'][spellIt]
+                let currentSpell = response.data['spells'][spellIt];
 
                 if (response.data['spells'].hasOwnProperty(spellIt)){
                     let spell = response.data['spells'][spellIt];
@@ -74,6 +45,7 @@ define([], function () {
                             spell['effectiveCastTime'] = spell['castTime'] ;
                         }
                         rank['castCoefficient'] = spell['effectiveCastTime'] / 3.5;
+
                         if (spell.name === 'Renew'){
                             rank['castCoefficient'] = 1;
                         }
@@ -87,28 +59,6 @@ define([], function () {
 
                     output['spells'].push(spell);
                 }
-
-                $scope.graphs[currentSpell.name] = {
-                  'series': [],
-                  'data': []
-                };
-
-                // Setting series
-                let rankIndex = 0;
-                for (let rank in response.data['spells'][spellIt]['ranks']){
-                    $scope.graphs[currentSpell.name].series.push("RANK " + response.data['spells'][spellIt]['ranks'][rank]['rank'])
-
-                    let rankValues = []
-                    // Setting up data
-                    for (let i=0;i<=1000;i+=10){
-                        let HPM = $scope.computeHPSM(i, response.data['spells'][spellIt], rankIndex)
-                        rankValues.push(HPM);
-                    }
-                    $scope.graphs[currentSpell.name].data.push(rankValues);
-                    rankIndex ++;
-
-                }
-                console.log($scope.graphs);
             }
 
             $scope.data = output;
@@ -122,6 +72,18 @@ define([], function () {
         $scope.computeHPSM = function(spellPower, spell, rank){
             let rankValue = spell['ranks'][rank];
             return ((rankValue['flat'] + (rankValue['effectiveCoefficient'] * spellPower)) / spell['effectiveCastTime'] ) / rankValue['cost']
+        };
+
+        $scope.computeHPS = function(spellPower, spell, rank){
+            let rankValue = spell['ranks'][rank];
+            return ((rankValue['flat'] + (rankValue['effectiveCoefficient'] * spellPower)) / spell['effectiveCastTime'] )
+        };
+
+        $scope.computeSpecial = function(spellPower, spell, rank, coefficient) {
+            let HPM = $scope.computeHPM(spellPower, spell, rank);
+            let HPS = $scope.computeHPS(spellPower, spell, rank);
+
+            return Math.pow(HPM, coefficient[0]) * Math.pow(Math.log10(HPS), coefficient[1]);
         }
 
 

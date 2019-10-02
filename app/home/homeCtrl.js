c87d3cece9cc18ba1e85d25562f25f343778ac3f
Terazus <batistadominique@hotmail.com>
2019-10-02 04:40:12 -0700

    some corrections

--- app/home/homeCtrl.js
+++ app/home/homeCtrl.js
@@ -18,7 +18,7 @@ define(['app'], function (app) {
         $scope.computeDetails = {
             "HPS": "HPS = Total healing done / cast time",
             "HPM": "HPM = Total healing done / mana cost",
-            "HPS/M": "HPS/M (HPM generate per point of mana used) = (Total healing done / cast time) / mana cost",
+            "HPS/M": "HPS/M (HPM generated per point of mana used) = (Total healing done / cast time) / mana cost",
             "HES": "HES = log(squareRoot(HPS*HPM))"
         }
 
